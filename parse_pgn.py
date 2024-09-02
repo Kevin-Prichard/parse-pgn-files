382ff7f984830232ad1367534ab748cf2b3f0791
kprichard <kprichard@gmail.com>
2024-09-02 13:07:39 -0700

    - parse_pgn.py: restructured to parallelize with multiprocessing.Process and Queue, with one PGN reader process (process_history()) feeding multiple mp.Process children (pgn_worker) via mp.Queues from which they consume PGNs to parse and count moves (and later we'll extend for building opening graphs) - parse_pgn.py: added options, --limit/-l for limiting the overall run to -l number of PGNs before terminating; --processes/-p to set the number of children (defaults to cpu core count); --queue_limit/-q limits queue depth to slow the reader's roll (default 250) - added console messaging to keep us informed of progress; with -p 18 processes on a 6 core i7 it takes about 1.667 minutes to parse 100,000 games; -p 12 it's 1.64 minutes, and 2.0 minutes with -p 6 processes, so there is some efficiency gained by running a multiple of processes to cores. Ran again with -p 24 processes and it got no more efficient than -p 12, but no worse... - not currently invoking move_parser, as we don't need to break down moves at this stage (and may never need to, if we're just building move graphs and not actually invoking moves on a headless board) - got it to build with gcc/cython3, and performance actually got worse by around 15% - implemented logging, which when set to logging.WARNING shaved off a couple seconds

--- parse_pgn.py
+++ parse_pgn.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+#cython: language_level=3
 
 import argparse
-import json
-import time
 from collections import defaultdict as dd
 from enum import Enum
+import json
 import logging
+import multiprocessing as mp
+from multiprocessing import Process, Queue
 import re
 import sys
+import time
 from typing import List, Tuple, Callable, Text, Dict
 
 from pgn_parser import pgn_file
@@ -16,6 +19,7 @@ from parsita import Success
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
 NAUGHTY_CHARS = re.compile(b'[^\n -~]+')
@@ -44,34 +48,36 @@ class ArgumentParser(argparse.ArgumentParser):
         self.print_help()
 
 
-def get_args(argv: List[str]) -> Tuple[
-        argparse.Namespace, ArgumentParser]:
+def get_args(argv: List[str]) -> Tuple[argparse.Namespace, ArgumentParser]:
     parser = ArgumentParser(
         prog='./parse_pgn.py',
         description='Parse PGN file(s) and stream(s): '
                     'parse PGNs and pass the resulting '
                     'parse tree to a function.')
     parser.add_argument('--file', '-f', dest='pgn_path',
-                        type = str, action = 'store', default = '-')
+                        type=str, action='store', default='-')
     parser.add_argument('--output', '-o', dest='out_path',
-                        type = str, action = 'store', default = '/dev/stdout')
+                        type=str, action='store', default='/dev/stdout')
+    parser.add_argument('--processes', '-p', dest='process_count',
+                        type=int, action='store', default=mp.cpu_count())
+    parser.add_argument('--limit', '-l', dest='pgn_limit',
+                        type=int, action='store', default=0)
+    parser.add_argument('--queue_limit', '-q', dest='queue_limit',
+                        type=int, action='store', default=250)
     return parser.parse_args(argv), parser
 
 
-class PGNParser:
+class PGNStreamSlicer:
     def __init__(self, pgn_path: str, parse_cb: Callable[[Dict], None] = None):
         self.pgn_path = pgn_path
-        self.pgn_file = open(self.pgn_path, 'rb')  # PGNs are UTF-8 encoded, e.g. "Réti Opening"
+        self.pgn_file = open(self.pgn_path, 'rb')  # rb: PGNs are UTF-8 encoded, e.g. "Réti Opening"
         self.parse_cb = parse_cb
         self.result = None
 
     def next(self):
-        # Saw off PGN hunks
+        # Saw off PGN hunks and hand them back to caller
         buf = []
         this_type, prev_type = None, None
-        count = 0
-        start_time = last_time = time.time()
-        todo = 34869171
         keep_going = True
         while keep_going:
             line = self.pgn_file.readline()
@@ -86,18 +92,6 @@ class PGNParser:
                 final_utf8 = NAUGHTY_CHARS.sub(
                     b'_', final, 999).decode('utf-8')
                 yield final_utf8
-                count += 1
-                if count % 100 == 0:
-                    this_time = time.time()
-                    # seconds_per_k = this_time - last_time
-                    time_per_k = (this_time - start_time) / (count / 1000)
-                    left = todo - count
-                    time_left = time_per_k * (left / 1000)
-                    print(f"count: {count}, "
-                          f"{round(time_per_k, 1)} sec/k, "
-                          f"left: {left:,}, "
-                          f"time left: {round(time_left / 3600, 2)} hrs")
-                    # last_time = this_time
                 buf.clear()
                 buf = [line[:-1]]
             else:
@@ -106,55 +100,157 @@ class PGNParser:
             if this_type is not PGNParts.NEWLINE:
                 prev_type = this_type
 
-    @classmethod
-    def parse(cls, pgn: Text):
-        return pgn_file.parse(pgn)
 
-    def stream_pgns(self):
-        for pgn in self.next():
-            # print(pgn,"\n\n")
-            result = pgn_file.parse(pgn)  # PGNParser.parse(pgn)
-            if isinstance(result, Success):
-                result = result.unwrap()
-                if self.parse_cb:
-                    self.parse_cb(result)
-                else:
-                    # There should only be one PGN per result, as get_pgn()
-                    # segments the file or stream into single PGNs.
-                    yield result[0]
-            else:
-                logger.error(f"Error parsing PGN: {pgn}")
-                logger.error(result)
+start_time = last_time = time.time()
+todo = 34869171
+
+m = mp.Manager()
 
 
-def main(argv):
-    args, parser = get_args(argv)
-    parser = PGNParser(pgn_path=args.pgn_path)
-    move_stats = dd(int)
-    mc = 0
-    for pgn in parser.stream_pgns():
-        for num, a_move in enumerate(pgn['game']['moves']):
+def handle_pgn(pgn, results: dict):
+    result = pgn_file.parse(pgn)
+    if isinstance(result, Success):
+        result = result.unwrap()[0]
+        results['gcount'] += 1
+
+        for num, a_move in enumerate(result['game']['moves']):
             try:
                 if a_move:
-                    mc += 1
+                    results['mcount'] += 1
                     if a_move['white']:
-                        move_stats[a_move['white'][0]] += 1
+                        if a_move['white'][0] in results["moves"]:
+                            results["moves"][a_move['white'][0]] += 1
+                        else:
+                            results["moves"][a_move['white'][0]] = 1
                     else:
                         logger.error("empty white move! at {num} in {pgn}")
                     if a_move['black']:
-                        move_stats[a_move['black'][0]] += 1
+                        if a_move['black'][0] in results["moves"]:
+                            results["moves"][a_move['black'][0]] += 1
+                        else:
+                            results["moves"][a_move['black'][0]] = 1
                 else:
                     logger.error("empty move! at {num} in {pgn}")
             except Exception as e:
                 logger.exception(
                     "Exception whilst combining moves: %s", a_move, e)
+    else:
+        logger.error(f"Error parsing PGN: {pgn}")
+        logger.error(result)
+
+
+def pgn_worker(queue: Queue, queue_id: Text, process_count: int):
+    logging.info("PROCESSQ: %s, %s -- %%%%%%%%%%%%",
+                 str(queue), str(queue_id))
+    work_count = 0
+    results = dict({"gcount": 0, "mcount": 0, "moves": dict()})
+    while True:
+        time.sleep(0.001)
+        qsize = queue.qsize()
+        if qsize:
+            pgn = queue.get()
+            if pgn is None:
+                queue.put(json.dumps(results))
+                break
+            handle_pgn(pgn, results)
+            work_count += 1
+            if work_count % 500 == 0:
+                logger.info("working: %s, %d, %d", queue_id, qsize, len(pgn))
+
+        if results['gcount'] % 1000 == 0:
+            count = results['gcount']
+            this_time = time.time()
+            time_per_k = (this_time - start_time) / (count / 1000)
+            left = (todo / process_count) - count
+            time_left = time_per_k * (left / 1000)
+            logger.info(f"count: {count}, "
+                          f"{round(time_per_k, 1)} sec/k, "
+                          f"left: {left:,}, "
+                          f"time left: {round(time_left / 3600, 2)} hrs, %s",
+                          str(mp.current_process()))
+
+
+def process_history(pgn_parser: PGNStreamSlicer, process_count: int, pgn_limit: int, queue_limit: int):
+    queues = dict()
+    processes = []
+    process_to_queue = dict()
+    mgr = mp.Manager()
+    for i in range(process_count):
+        queue_id = f"queue_{i}"
+        queues[queue_id] = this_q = mp.Queue()
+        x = Process(
+            target=pgn_worker,
+            kwargs=dict(queue=this_q,
+                        queue_id=queue_id,
+                        process_count=process_count)
+        )
+        process_to_queue[x] = this_q
+        processes.append(x)
+        logger.debug("process created: %s, %s", str(this_q), str(x))
+
+    logger.debug("Queues: %s", [", ".join(str(q) for q in queues)])
+    started = False
+    process_idx = 0
+    queue_id = f"queue_{process_idx}"
+    this_q = queues[queue_id]
+    for pgn_num, pgn in enumerate(pgn_parser.next()):
+        if pgn_num % 1000 == 0:
+            logger.warning("pgn_num %d", pgn_num)
+        if pgn_limit and pgn_num > pgn_limit:
+            break
+        loop_ctr = 0
+        while True:
+            if this_q.qsize() < queue_limit:
+                this_q.put(pgn)
+                break
+            logger.info("Flipped the lid: %d, %d, %s", this_q.qsize(), pgn_num, process_idx)
+            loop_ctr += 1
+            process_idx = (process_idx + 1) % process_count
+            queue_id = f"queue_{process_idx}"
+            this_q = queues[queue_id]
+            if loop_ctr > process_count:
+                if not started:
+                    for process in processes:
+                        logger.debug("starting: %s", str(process))
+                        process.start()
+                    started = True
+                logger.info("JAMMED UP - all queues full, waiting to clear"
+                            "%d, %s, %d, %s",
+                      pgn_num, process_idx, loop_ctr, "*"*80)
+                time.sleep(1)
+
+    for q in queues.values():
+        q.put(None)
+
+    merged = dict(gcount=0, mcount=0, moves=dd(int))
+    for p in processes:
+        p.join()
+        results = json.loads(process_to_queue[p].get())
+        merged['gcount'] += results['gcount']
+        merged['mcount'] += results['mcount']
+        for move, count in results['moves'].items():
+            merged['moves'][move] += count
+
+    mgr.shutdown()
+
+    return merged
+
+
+def main(argv):
+    args, parser = get_args(argv)
+    parser = PGNStreamSlicer(pgn_path=args.pgn_path)
+    move_stats = process_history(parser, args.process_count, args.pgn_limit, args.queue_limit)
+
     with open(args.out_path, "w") as f:
         f.write(json.dumps({
-            "move_count": mc,
+            "game_count": move_stats['gcount'],
+            "move_count": move_stats['mcount'],
             "moves": sorted(move_stats.keys()),
-            "stats": move_stats
+            "stats": {k: v for k, v in move_stats.items()},
         }))
 
+    return 0.0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv[1:])
