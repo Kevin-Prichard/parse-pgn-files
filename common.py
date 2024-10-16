6f515bd9689f1ccfc8f4d818597c9a4dfe532d9f
kprichard <kprichard@gmail.com>
2024-10-16 13:18:22 -0700

    - common.py: add CHECKS_RX for detecting checks, in addition to absolute pins

--- common.py
+++ common.py
@@ -5,6 +5,7 @@ GLOBAL_LOGGING_LEVEL = logging.INFO
 SIDES = ('W', 'B')
 OPPO = {'W': 'B', 'B': 'W'}
 PINS_RX = dict()
+CHECKS_RX = dict()
 
 for side in SIDES:
     PINS_RX[side] = re.compile(
@@ -17,6 +18,19 @@ for side in SIDES:
         # ONE opponent piece
         f"((R|N|B|Q|P){OPPO[side]})"
         # zero or more empty squares after opponent piece
+        f"(  )*"
+        # opponent king
+        f"K{OPPO[side]}"
+        # end of string
+        "$"
+    )
+
+    CHECKS_RX[side] = re.compile(
+        # beginning of string
+        "^"
+        # Current piece under contemplation
+        f"(Q|R|B){side}"
+        # zero or more empty squares before opponent piece
         f"(  )*"
         # opponent king
         f"K{OPPO[side]}"
